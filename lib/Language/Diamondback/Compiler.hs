2f6e3d0e085f0203a986ec302354a837989fca01
Sirajer <sirajghouse@gmail.com>
2018-03-03 01:38:07 -0800

    doesn't work but ill take it

--- lib/Language/Diamondback/Compiler.hs
+++ lib/Language/Diamondback/Compiler.hs
@@ -116,7 +116,7 @@ compileEnv env (Prim2 o v1 v2 l) = compilePrim2 l env o v1 v2
 compileEnv env (If v e1 e2 l)    = compileIf l env v e1 e2
 
 compileEnv env (App f vs l) | annTail l = tailcall (DefFun f) (param env <$> vs)
-							| otherwise = let args = (param env v | v <- vs) in 
+							| otherwise = let args = (param env <$> vs) in 
 										  [ISub (Reg ESP) (Const (4 * (length args)))]
 										  ++ [IPush a | a <- reverse args ]
 										  ++ [ICall (DefFun f), IAdd (Reg ESP) (Const (4 * (length args)))]
